3b1dac727ae02374b465b208a7f336491c0fc409
sseezov <sseezov@mail.ru>
2022-04-26 23:07:43 +0700

    done with style issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import data from "./data";
-import Question from "./Question";
+import SingleQuestion from "./Question";
+
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
-    <div>
-      <Question />
-    </div>
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
 
